7a57dca75a60fa851361dcc34e5b634b03001ced
subhadip <ytsubhadip0099@gmail.com>
2026-03-05 22:24:45 +0530

    mp3 to json chunk create

--- mp3_to_json.py
+++ mp3_to_json.py
@@ -4,14 +4,14 @@ import os
 #load whisper large-v2 model
 model = whisper.load_model("large-v2")
 
-audios = os.listdir("audio")
+audios = os.listdir("audios")
 for audio in audios:
     number = audio.split("_")[-1].split(".")[0]
-    title = audio.split("_Python")[0]
+    title = audio.split("_")[0]
     print(f"load: {audio}")
     print("pocess..........")
 
-    result = model.transcribe(audio=f"audio/{audio}",
+    result = model.transcribe(audio=f"audios/{audio}",
                           language="hi",
                           task="translate",   
                           word_timestamps=False)
@@ -27,7 +27,7 @@ for audio in audios:
         
     chunks_with_metadata = {"chunks": chunk, "text":result["text"]}
 
-    with open(f"jsons/{number}_{title}.json","w") as f:
+    with open(f"json/{number}_{title}.json","w") as f:
         json.dump(chunks_with_metadata,f)
     print("successfull...")
 
--- video_to_mp3.py
+++ video_to_mp3.py
@@ -1,11 +1,12 @@
 import os
 import subprocess
 
-files = os.listdir("videos")
+files = os.listdir("video")
 for file in files:
-    tutorial_number = (file.split('-_')[1].split('_')[1])
-    file_name = file.split("-_")[0]
+    tutorial_number = (file.split('-')[0])
+    file_name = file.split('｜')[0].split('-',1)[1].strip()
+
+    subprocess.run(["ffmpeg","-i",f"video/{file}",f"audios/{file_name}_{tutorial_number}.mp3"])
     
-    subprocess.run(["ffmpeg","-i",f"videos/{file}",f"audio/{file_name}{tutorial_number}.mp3"])
-    print("successfull")
+    
     
